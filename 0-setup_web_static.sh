c0b527d4c1c3325de40c8e472c321c28c7d15b0e
desmends27 <desmondmends27@gmail.com>
2023-07-06 18:20:16 +0000

    minor update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 #Prepare the webservers for the deployment of web_static
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo -hR chown ubuntu:ubuntu /data/
