2b41a12b010c6a27b468b6002e5c266279133c9e
desmends27 <desmondmends27@gmail.com>
2023-07-06 18:25:55 +0000

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo -hR chown ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
