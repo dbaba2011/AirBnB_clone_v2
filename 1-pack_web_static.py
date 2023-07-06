460cd729afa7f582bdfaefe96d7fc6563c3ae169
desmends27 <desmondmends27@gmail.com>
2023-07-06 22:18:21 +0000

    indent

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,13 +10,13 @@ from datetime import datetime
 
 def do_pack():
     """ Generate a .tgz archive file from the contents in web_static """
-    date = datatime.now().strftime("%Y%m%d%H%M%S")
-    try:
-        #check if the versions exits
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_path = "versions/web_static_{}.tgz".format(date)
-        local("tar -czvf {} web_static".format(file_path))
-        return file_name
+        try:
+            date = datetime.now().strftime("%Y%m%d%H%M%S")
+            #check if the versions exits
+            if isdir("versions") is False:
+                local("mkdir versions")
+            file_path = "versions/web_static_{}.tgz".format(date)
+            local("tar -czvf {} web_static".format(file_path))
+            return file_name
     except:
         return None
