218bc0a7cbf22a6c0a655755c25b78a3c3475810
dbaba2011 <dbaba2011@yahoo.com.com>
2023-07-09 05:30:25 -0700

    My AirBnB_clone_v2 ALX project tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,66 +1,65 @@
 #!/usr/bin/python3
-""" deploy code to the server """
-import os.path
-from fabric.api import env, put, run, local
-from datetime import datetime
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-
-env.hosts = ["18.234.168.215", "52.72.71.162"]
-
-
-def do_pack():
-    """ pack web_static folder into a .tgz file """
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
+from fabric.api import *
+import os
+env.hosts = ['54.172.102.75', '54.160.119.245']
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to web servers
-    Args:
-        archive_path(str): Path to the archive to distribute
-    Return: True if succeed otherwise false """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    """Archive distributor for f deployment"""
+    try:
+        try:
+            if os.path.exists(archive_path):
+                arc_tgz = archive_path.split("/")
+                arg_save = arc_tgz[1]
+                arc_tgz = arc_tgz[1].split('.')
+                arc_tgz = arc_tgz[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+                """Upload archive to the server"""
+                put(archive_path, '/tmp')
+
+                """Save folder paths in variables"""
+                uncomp_fold = '/data/web_static/releases/{}'.format(arc_tgz)
+                tmp_location = '/tmp/{}'.format(arg_save)
+
+                """Run remote commands on the server"""
+                run('mkdir -p {}'.format(uncomp_fold))
+                run('tar -xvzf {} -C {}'.format(tmp_location, uncomp_fold))
+                run('rm {}'.format(tmp_location))
+                run('mv {}/web_static/* {}'.format(uncomp_fold, uncomp_fold))
+                run('rm -rf {}/web_static'.format(uncomp_fold))
+                run('rm -rf /data/web_static/current')
+                run('ln -sf {} /data/web_static/current'.format(uncomp_fold))
+                run('sudo service nginx restart')
+                return True
+            else:
+                print('File does not exist')
+                return False
+        except Exception as err:
+            print(err)
+            return False
+    except Exception:
+        print('Error')
         return False
-    if run(
-            "rm -rf /data/web_static/releases/{}/".format(
-                name)).failed is True:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-    if run(
-            "mkdir -p /data/web_static/releases/{}/".format(
-                name)).failed is True:
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
         return False
-    if run(
-            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-                file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run(
-            "rm -rf /data/web_static/releases/{}/web_static".format(
-                name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(name)).failed is True:
-        return False
-    return True
