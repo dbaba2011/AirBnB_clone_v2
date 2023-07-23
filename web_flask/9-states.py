56e6c44b209b9c87dae91b41d06ea3c662fd1b4c
dbaba2011 <dbaba2011@yahoo.com>
2023-07-23 07:10:45 -0700

    a script that starts a Flask web application:

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,17 +12,24 @@ from models.amenity import Amenity
 from models.review import Review
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ display HTML page with list of cities """
-    states = storage.all(classes["State"]).values()
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id=None):
+    """ display HTML page with list of states """
+    all_states = storage.all(State)
+    if id:
+        states = all_states.get('State.{}'.format(id))
+    else:
+        states = all_states.values()
     # ^ fetches states data from storage engine, then in line below,
     # those states are passed into the template
-    return render_template('8-cities_by_states.html', states=states)
+    return render_template('9-states.html', states=states)
 
 
 @app.teardown_appcontext
